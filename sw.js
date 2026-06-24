09eba468010686f7039a691d10218947268feb0a
vergam <156515702+vergam@users.noreply.github.com>
2026-06-24 16:18:44 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'fantasy-tour-v5';
+const DATA_FILES = ['data/classifications.json', 'data/riders.json'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  const isData = DATA_FILES.some(f => url.pathname.endsWith(f));
+  if (isData) {
+    e.respondWith(
+      fetch(e.request.clone()).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  }
+});
